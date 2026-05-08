23c32fa2edadfbf7dcd38e44683f9309bca18c3c
Boris-Kost <bkostogukaylov87@gmail.com>
2026-05-08 11:27:15 +0500

    fix: add websocket proxy for local development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
         target: 'http://127.0.0.1:5001',
         changeOrigin: true,
       },
+      '/socket.io': {
+        target: 'ws://127.0.0.1:5001',
+        ws: true,
+        changeOrigin: true,
+      },
     },
   },
   build: {
